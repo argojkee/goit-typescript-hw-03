22b29671847d25b5e1ce54ac0b97de67efd753ec
argojkee <maksymhaidabura@gmail.com>
2023-11-13 21:03:30 +0200

    full hw

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,67 @@
+interface IKey {
+  getSignature(): number;
+}
+
+interface IPerson {
+  getKey(): IKey;
+}
+
+interface IHouse {
+  door: boolean;
+  key: IKey;
+  tenants: IPerson[];
+  comeIn(person: IPerson): void;
+  openDoor(key: IKey): void;
+}
+
+class Key implements IKey {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person implements IPerson {
+  constructor(private key: IKey) {}
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House implements IHouse {
+  door: boolean = false;
+  tenants: IPerson[] = [];
+
+  constructor(public key: IKey) {
+    this.key = key;
+  }
+
+  comeIn(person: IPerson) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  abstract openDoor(key: IKey): void;
+}
+
+class MyHouse extends House {
+  constructor(key: IKey) {
+    super(key);
+  }
+
+  openDoor(key: IKey) {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +70,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
